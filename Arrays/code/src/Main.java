39987db37c3924f2ff4df537692af1e9a78a0117
Pushkar Verma <pushkar7397@gmail.com>
2022-09-06 10:05:40 +0530

    Java Arrays
    Start with Java Arrays

---
+++ Arrays/code/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "Pushkar";
+
+        // Q: store 5 roll nos.
+
+
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        // store 5 roll nos.
+//        int[] rnos = new int[5];
+//        // or directly
+//        int[] rnos2 = {23, 12, 45, 32, 15};
+
+        int[] ros; // declaration of array, ros is getting defined in the stack
+        ros = new int[5]; // initialisation, actually here object is being created in the heap memory
+        
+
+
+    }
+}
