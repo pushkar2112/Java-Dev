4bebaf74bec0fa62cce130249f83406e8657fc09
Pushkar Verma <pushkar7397@gmail.com>
2022-10-04 22:29:26 +0530

    Code cleanup

--- binary-search/code/BinarySearch/src/Test.java
+++ binary-search/code/BinarySearch/src/Test.java
@@ -5,20 +5,36 @@ import java.util.Objects;
 
 public class Test {
     public static void main(String[] args) {
-        int[] mat = {1,1,2};
-        int k = 1;
-        int ans = removeDuplicates(mat);
+        List<List<Integer>> ans = generate(5);
         System.out.println(ans);
     }
 
-    static int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+    static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> answer = new ArrayList<Integer>(i+1);
+            if (i == 0) {
+                answer.add(1);
+                triangle.add(answer);
+                continue;
             }
+            answer.add(1);
+            answer.add(i, 1);
+
+            for (int j = 0; j < i; j++) {
+                if (j == 0) {
+                    // ans[i][j] = 1;
+                    continue;
+                } else {
+                    // int sum = ans[i-1][j-1] + ans[i-1][j];
+                    int sum = triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j);
+                    answer.add(j, sum);
+                }
+            }
+            triangle.add(answer);
         }
-        return i + 1;
+
+        return triangle;
     }
 }
